814292b8e37d3cd760a18a44fbf452d8d36b267f
Victor Shang <30793693+victorshang@users.noreply.github.com>
2020-09-13 21:13:52 +0800

    更新 esp8266.ts

--- esp8266.ts
+++ esp8266.ts
@@ -60,7 +60,6 @@ namespace ESP8266 {
         sendAT("AT+CWMODE=1",1000) // 设置为STA模式
         last_cmd_successful= waitResponse("OK")
         sendAT("AT+RST", 2000) // 重启
-        last_cmd_successful= waitResponse("OK")
         sendAT("AT+CWJAP=\"" + ssid + "\",\"" + pw + "\"",0) // 连接到WIFI
         last_cmd_successful= waitResponse("OK")
         wifi_connected =last_cmd_successful
