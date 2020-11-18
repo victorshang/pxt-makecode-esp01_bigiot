498342a6aa4bbd6ee464d017c7ba9aacef5ba144
Victor Shang <30793693+victorshang@users.noreply.github.com>
2020-11-18 22:24:58 +0800

    更新 pxt.json, main.ts, main.py

---
+++ main.ts
@@ -0,0 +1,56 @@
+let 命令 = ""
+basic.pause(2000)
+basic.showIcon(IconNames.Heart)
+ESP8266.connectWifi(SerialPin.P2, SerialPin.P1, BaudRate.BaudRate115200, "Redmi_Mini", "0987654323")
+while (!ESP8266.isWifiConnected()) {
+    basic.pause(100)
+}
+basic.showString("W")
+Bigiot_net.connectToBigiotServer("www.bigiot.net", 8181)
+basic.showString("L")
+Bigiot_net.checkoutBigiot("12844", "fb0b41e34")
+basic.showString("O")
+Bigiot_net.checkinBigiot("12844", "fb0b41e34")
+basic.showString("I")
+basic.forever(function on_forever() {
+    
+    basic.pause(200)
+    if (Bigiot_net.waitforCommand(3000)) {
+        命令 = Bigiot_net.lastCmd()
+        basic.showString(Bigiot_net.lastCmd(), 50)
+        if (命令 == "on") {
+            pins.digitalWritePin(DigitalPin.P16, 1)
+        }
+        
+        if (命令 == "off") {
+            pins.digitalWritePin(DigitalPin.P16, 0)
+        }
+        
+        if (命令 == "play") {
+            music.setVolume(37)
+            music.startMelody(music.builtInMelody(Melodies.Punchline), MelodyOptions.ForeverInBackground)
+        }
+        
+        if (命令 == "stop") {
+            music.stopMelody(MelodyStopOptions.All)
+        }
+        
+        if (命令 == "down") {
+            control.reset()
+        }
+        
+    }
+    
+})
+basic.forever(function on_forever2() {
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+    basic.clearScreen()
+    Bigiot_net.updateBigiot2("12844", "11251", convertToText(input.acceleration(Dimension.Y)), "11345", convertToText(input.acceleration(Dimension.X)))
+    basic.pause(5000)
+})
